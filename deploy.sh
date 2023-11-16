7d38f46b2514fbd31ed1e200df87856dac9aa58b
bbj14 <40915287+bbj14@users.noreply.github.com>
2023-11-16 19:08:37 +0900

    conf設定

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
 set -xe
-cd golang && make && cd -
 now=$(date +%Y%m%d-%H%M%S)
+cd go && GOOS=linux GOARCH=amd64 go build -o isuconquest && cd -
 
 for h in 1
 do
-   rsync -av  --exclude '.git' ./ isucon${h}:/home/isucon/isucon12-final/webapp/
-   ssh isucon${h} sudo systemctl restart isuconquest.go
-   ssh isucon${h} sudo cp /home/isucon/isucon12-final/webapp/nginx.conf /etc/nginx/nginx.conf
-   ssh isucon${h} sudo cp /home/isucon/isucon12-final/webapp/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
+   rsync -av  --exclude '.git' ./ isucon:/home/isucon/isucon12-final/webapp/
+   ssh isucon mv /home/isucon/isucon12-final/webapp/go/isuconquest /home/isucon/webapp/go/isuconquest
+   ssh isucon sudo systemctl restart isuconquest.go
+   ssh isucon sudo cp /home/isucon/isucon12-final/webapp/nginx.conf /etc/nginx/nginx.conf
+   ssh isucon sudo cp /home/isucon/isucon12-final/webapp/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 done
 
 # nginx
 for h in 1
 do
-   ssh isucon${h} sudo touch /var/log/nginx/access.log
-   ssh isucon${h} sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.$now
-   ssh isucon${h} sudo systemctl restart nginx
+   ssh isucon sudo touch /var/log/nginx/access.log
+   ssh isucon sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.$now
+   ssh isucon sudo systemctl restart nginx
 done
 
 # mysql
 for h in 1
 do
-   ssh isucon${h} sudo touch /var/log/mysql/mysql-slow.log
-   ssh isucon${h} sudo mv /var/log/mysql/mysql-slow.log /var/log/mysql/mysql-slow.log.$now
-   ssh isucon${h} sudo mysqladmin flush-logs
-   ssh isucon${h} sudo systemctl restart mysql
+   ssh isucon sudo touch /var/log/mysql/mysql-slow.log
+   ssh isucon sudo mv /var/log/mysql/mysql-slow.log /var/log/mysql/mysql-slow.log.$now
+   ssh isucon sudo mysqladmin flush-logs
+   ssh isucon sudo systemctl restart mysql
 done
